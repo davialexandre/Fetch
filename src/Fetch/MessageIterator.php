7fb2e61b145259c97369db9652f0269d4f40759f
Davi Alexandre <davi@davialexandre.com.br>
2014-01-22 17:37:21 -0200

    Fixing rewind and next in the MessageIterator.
    Those methods should only update the iterator position and return void.
    
    I have also added doc comments to all the MessageIterator methods.

--- src/Fetch/MessageIterator.php
+++ src/Fetch/MessageIterator.php
@@ -19,6 +19,14 @@ class MessageIterator implements \Iterator, \Countable {
      */
     protected $numberOfMessages;
 
+    /**
+     * Create a new MessageIterator.
+     *
+     * If size is bigger than the number of messages in the server, the later will be used.
+     *
+     * @param Server $server a Server instance which will be used to fetch the messages
+     * @param int|null $size the max number of messages in the iterator. If null, the number of messages in the server will be used
+     */
     public function __construct($server, $size = null) {
         $this->server = $server;
         $this->position = 1;
@@ -28,33 +36,58 @@ class MessageIterator implements \Iterator, \Countable {
         }
     }
 
+    /**
+     * Returns the current Message for the iterator
+     *
+     * @return bool|Message|mixed
+     */
     public function current()
     {
         return $this->server->getMessageBySequenceNumber($this->position);
     }
 
+    /**
+     * Moves the iterator to the next position
+     */
     public function next()
     {
         $this->position++;
-        return $this->server->getMessageBySequenceNumber($this->position);
     }
 
+    /**
+     * Returns the current position of the iterator
+     *
+     * @return int
+     */
     public function key()
     {
         return $this->position;
     }
 
+    /**
+     * Checks if the current position is valid.
+     * Since message sequences starts with 1, it can't be 0 or less and it can' be higher than the number of messages
+     *
+     * @return bool
+     */
     public function valid()
     {
         return $this->position > 0 && $this->position <= $this->numberOfMessages;
     }
 
+    /**
+     * Moves the iterator to the first position
+     */
     public function rewind()
     {
         $this->position = 1;
-        return $this->server->getMessageBySequenceNumber($this->position);
     }
 
+    /**
+     * Returns the number of messages in the iterator
+     *
+     * @return int
+     */
     public function count()
     {
         return $this->numberOfMessages;
